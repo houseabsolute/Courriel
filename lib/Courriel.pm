f398f4cd4079d6557d49f5befd0da82244018a16
Dave Rolsky <autarch@urth.org>
2019-04-21 13:50:25 -0500

    Remove donations pod section as this is now added via pod weaver

--- lib/Courriel.pm
+++ lib/Courriel.pm
@@ -761,20 +761,3 @@ Please report any bugs or feature requests to C<bug-courriel@rt.cpan.org>, or
 through the web interface at L<http://rt.cpan.org>.  I will be notified, and
 then you'll automatically be notified of progress on your bug as I make
 changes.
-
-=head1 DONATIONS
-
-If you'd like to thank me for the work I've done on this module, please
-consider making a "donation" to me via PayPal. I spend a lot of free time
-creating free software, and would appreciate any support you'd care to offer.
-
-Please note that B<I am not suggesting that you must do this> in order for me
-to continue working on this particular software. I will continue to do so,
-inasmuch as I have in the past, for as long as it interests me.
-
-Similarly, a donation made in this way will probably not make me work on this
-software much more, unless I get so many donations that I can consider working
-on free software full time, which seems unlikely at best.
-
-To donate, log into PayPal and send money to autarch@urth.org or use the
-button on this page: L<http://www.urth.org/~autarch/fs-donation.html>
